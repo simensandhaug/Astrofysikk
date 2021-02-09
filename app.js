14ded33268f230b8b80cdd496d85e0b3d791cee8
simensandhaug <70633406+simensandhaug@users.noreply.github.com>
2021-02-09 13:47:45 +0100

    foundation

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("headerContainer").addEventListener("click", () => {
+    location.href = 'index.html';
+});
+
+document.getElementById("stjerner").addEventListener("click", () => {
+    location.href = 'stjerner.html';
+});
