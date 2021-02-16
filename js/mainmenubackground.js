8975661524a5f5faa4b58c1e0489bce392a0ab97
simensandhaug <simen.sandhaug@gmail.com>
2021-02-16 23:56:15 +0100

    style endringer

---
+++ js/mainmenubackground.js
@@ -0,0 +1,29 @@
+let backgrounds = ['background1.jpeg', 'background2.jpeg', 'background3.jpeg'];
+
+document.querySelector(".container").style.backgroundImage = `url(../images/${backgrounds[Math.floor(Math.random() * backgrounds.length)]})`;
+
+let buttonMenuEl = document.getElementById("buttonMenu");
+let titleEl = document.getElementById("title");
+let count = 0;
+const toggleButtons = () => {
+    if(count%2 == 0) buttonMenuEl.style.visibility = "visible";
+    else buttonMenuEl.style.visibility = "hidden";
+    count++;
+}
+
+let header = "Astrofysikk";
+const writeHeader = () => {
+    let i = 0;
+    setInterval(() => {
+        if(i < header.length) titleEl.innerHTML += header.charAt(i);
+        else {
+            if(i%2 == 0) titleEl.innerHTML += "_";
+            else {
+                titleEl.innerHTML = titleEl.innerHTML.replace("_", "");
+            }
+        }
+        i++
+    }, 500);
+}
+writeHeader();
+
--- js/stjerner.js
+++ js/stjerner.js
@@ -1,32 +1,30 @@
-const contentContainer = document.getElementById("contentContainer")
+const body = document.querySelector("body");
 let stars;
-
 class Star {
     constructor(name, age, kelvin, distance, img, fact) {
         this.name = name;
         this.age = age;
         this.kelvin = kelvin,
-        this.distance = distance,
-        this.img = img,
-        this.fact = fact;
+            this.distance = distance,
+            this.img = img,
+            this.fact = fact;
     }
 
     showInfo() {
-        contentContainer.innerHTML = `<div id="headerContainer" onclick="location.href='stjerner.html'"><h1>Astrofysikk</h1><p>${this.name}</p><p>Trykk for å gå tilbake</p></div><br>`;
-        contentContainer.innerHTML += `<div class="starInfo"><h1>Info om ${this.name}</h1><br></div>`;
-        contentContainer.innerHTML += `<div class="starInfo"><h2>Alder: ${this.age} år</h2><h2>Fargetemperatur: ${this.kelvin} K</h2><h2>Avstand: ${this.distance}</h2><br><h2>λ<sub>topp:</sub> <br>2.9 ⋅ 10<sup>-3</sup> / ${this.kelvin} = ${Math.floor(((2.9*0.01)/this.kelvin)*Math.pow(10, 8))} nm<br><h2>Utstrålingstetthet:<br> 5.67 ⋅ 10<sup>-8</sup> ⋅ ${this.kelvin}<sup>4</sup> = ${Math.floor((Math.pow(5.67, -8) * Math.pow(this.kelvin,4))).toExponential(1).replace(/e\+?/, ' ⋅ 10<sup>')}</sup> W/m<sup>2</sup></h2><h2>Bilde</h2></h2><br><img class="starIMG" src="${this.img}"<br><p>Funfacts</p><p>${this.fact}</p></div>`
+        body.innerHTML += `<div id="contentContainer"></div>`
+        document.getElementById("contentContainer").innerHTML = `<div id="headerContainer" onclick="location.href='stjerner.html'"><h1>Astrofysikk</h1><p>${this.name}</p><p>Trykk for å gå tilbake</p></div><br>`;
+        document.getElementById("contentContainer").innerHTML += `<div class="starInfo"><h1>Info om ${this.name}</h1><br></div>`;
+        document.getElementById("contentContainer").innerHTML += `<div class="starInfo"><h2>Alder: ${this.age} år</h2><h2>Fargetemperatur: ${this.kelvin} K</h2><h2>Avstand: ${this.distance}</h2><br><h2>λ<sub>topp:</sub> <br>2.9 ⋅ 10<sup>-3</sup> / ${this.kelvin} = ${Math.floor(((2.9*0.01)/this.kelvin)*Math.pow(10, 8))} nm<br><h2>Utstrålingstetthet:<br> 5.67 ⋅ 10<sup>-8</sup> ⋅ ${this.kelvin}<sup>4</sup> = ${Math.floor((Math.pow(5.67, -8) * Math.pow(this.kelvin,4))).toExponential(1).replace(/e\+?/, ' ⋅ 10<sup>')}</sup> W/m<sup>2</sup></h2><h2>Bilde</h2></h2><br><img class="starIMG" src="${this.img}"<br><p>Funfacts</p><p>${this.fact}</p></div>`
     }
 }
-
 const showStar = id => {
     stars.forEach(star => {
-        if(id == star.name) {
+        if (id == star.name) {
             let temporary = new Star(star.name, star.age, star.kelvin, star.distance, star.img, star.fact);
             temporary.showInfo();
         }
     });
 }
-
 const loadPage = () => {
     stars = [{
             name: 'Betelgeuse',
@@ -45,8 +43,10 @@ const loadPage = () => {
             fact: 'Proxima Centauri er den stjernen som ligger nærmest solen. Den tilhører stjernebildet Kentauren, derav navnet Centauri. Den ligger i trippelstjernesystemet Alfa Centauri der den kretser rundt hovedstjernen Alfa Centauri med en omløpstid på ca. én million år. Den er en rød dverg, noe som betyr at den er relativt liten og "kjølig". Bildet over ble tatt av Hubble Teleskopet i 2013.'
         },
     ];
-    contentContainer.innerHTML = `<div id="headerContainer" onclick="location.href='index.html'"><h1>Astrofysikk</h1><p>Stjerneinfo</p><p>Trykk for å gå tilbake</p></div><br>`;
-    contentContainer.innerHTML += `<div id="starList"></div>`;
+    body.innerHTML += `<div id="contentContainer"></div>`
+    body.innerHTML += `<canvas id="panCanvas" width="1200px" height="1000px"></canvas>`
+    document.getElementById("contentContainer").innerHTML = `<div id="headerContainer" onclick="location.href='index.html'"><h1>Astrofysikk</h1><p>Stjerneinfo</p><p>Trykk for å gå tilbake</p></div><br>`;
+    document.getElementById("contentContainer").innerHTML += `<div id="starList"></div>`;
     stars.forEach(star => {
         document.getElementById("starList").innerHTML += `<button class="mainMenuBtn" id="${star.name}" onclick="showStar(this.id)">${star.name}</button><br>`;
     });
